5867d56df85945a641d24746b5b39a7def8bf049
Vijay Segunasi <vijay.s@proglint.com>
2026-08-21 14:29:03 +0530

    Added YOLOv8 pizza segmentation project

--- predict.py
+++ predict.py
@@ -5,21 +5,31 @@ model = YOLO(r"C:\Users\VijaySegunasi\runs\segment\train-4\weights\best.pt")
 results = model.predict(
     source=r"C:\Users\VijaySegunasi\pizza_yolo_model\test_images",
     save=True,
+    project=r"C:\Users\VijaySegunasi\pizza_yolo_model",
+    name="prediction_results",
     conf=0.25
 )
 
 with open("prediction_report.txt", "w") as f:
-    for r in results:
-        f.write(f"\nImage: {r.path}\n")
+    f.write(f"Total Images Tested: {len(results)}\n\n")
+
+    for i, r in enumerate(results, start=1):
+        f.write(f"Image {i}: {r.path}\n")
 
         if r.boxes is not None:
             for box in r.boxes:
                 cls = int(box.cls)
-                conf = float(box.conf)
+                confidence = float(box.conf)
 
                 class_name = model.names[cls]
 
                 f.write(
-                    f"Detected: {class_name}, "
-                    f"Confidence: {conf:.2f}\n"
+                    f"  - Detected: {class_name}, "
+                    f"Confidence: {confidence:.2%}\n"
                 )
+
+        f.write("\n")
+
+print("Prediction complete!")
+print("Images saved in: prediction_results")
+print("Report saved as: prediction_report.txt")
--- train.py
+++ train.py
@@ -6,6 +6,8 @@ model.train(
     data="data.yaml",
     epochs=10,
     batch=8,
-    imgsz=640
+    imgsz=640,
+    project="runs",
+    name="pizza_segmentation_new"
 )
 #C:\Users\VijaySegunasi\runs\segment\train-4
